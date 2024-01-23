54b2c68480a7f016741d966b44b598134a37fd53
Vini <vinirnadolny@gmail.com>
2024-01-23 18:09:08 -0300

    Added pieces images and board initial state

--- app/board.tsx
+++ app/board.tsx
@@ -1,11 +1,41 @@
+'use client'
+import { useState } from "react"
 import Square from "./square"
 
+// let piecesIds = {
+//     'none': 0,
+//     'pawn': 1,
+//     'knight': 2,
+//     'bishop': 3,
+//     'rook': 4,
+//     'queen': 5,
+//     'king': 6,
+// }
+
+const initialState = {
+    board: [-4,-2,-3,-5,-6,-3,-2,-4,
+            -1,-1,-1,-1,-1,-1,-1,-1,
+             0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0,
+             1, 1, 1, 1, 1, 1, 1, 1,
+             4, 2, 3 ,5 ,6 ,3, 2, 4]
+}
+
+interface Piece {
+    readonly color: -1 | 1
+    readonly type: 0 | 1 | 2 | 3 | 4 | 5 | 6 
+}
+
 export default function Board() {
+
+    const [board, setBoard] = useState<number[]>(initialState.board)
 
     function renderSquares(){
         let squares = new Array()
         for (let i = 0; i < 64; i++){
-            squares.push(<Square squareId={i} key={i} />)
+            squares.push(<Square pieceId={board[i].toString()} squareId={i} key={i} />)
         }
         return(squares)
     }
--- app/square.tsx
+++ app/square.tsx
@@ -1,10 +1,51 @@
+import Image, { StaticImageData } from 'next/image'
+import bbishop from '../public/pieces/b_bishop_png_128px.png'
+import bking from '../public/pieces/b_king_png_128px.png'
+import bknight from '../public/pieces/b_knight_png_128px.png'
+import bpawn from '../public/pieces/b_pawn_png_128px.png'
+import bqueen from '../public/pieces/b_queen_png_128px.png'
+import brook from '../public/pieces/b_rook_png_128px.png'
+import wbishop from '../public/pieces/w_bishop_png_128px.png'
+import wking from '../public/pieces/w_king_png_128px.png'
+import wknight from '../public/pieces/w_knight_png_128px.png'
+import wpawn from '../public/pieces/w_pawn_png_128px.png'
+import wqueen from '../public/pieces/w_queen_png_128px.png'
+import wrook from '../public/pieces/w_rook_png_128px.png'
+import empty from '../public/pieces/empty.png'
+
 type Props = {
-    squareId: number
+    squareId: number,
+    pieceId: string,
 }
 
-export default function Square({ squareId }: Props){
-    let color = (squareId + Math.floor(squareId / 8)) % 2 === 0 ? 'bg-slate-500' : 'bg-slate-800'
+const pieceDatabase = {
+    "0": empty,
+    "1": wpawn,
+    "2": wknight,
+    "3": wbishop,
+    "4": wrook,
+    "5": wqueen,
+    "6": wking,
+    "-1": bpawn,
+    "-2": bknight,
+    "-3": bbishop,
+    "-4": brook,
+    "-5": bqueen,
+    "-6": bking,
+}
+
+export default function Square({ squareId, pieceId }: Props){
+    let background = (squareId + Math.floor(squareId / 8)) % 2 === 0 ? 'bg-slate-500' : 'bg-slate-800'
+    let piece: StaticImageData = pieceDatabase[pieceId]
+
     return(
-        <div className={`${color} h-[102px] w-[102px]`} />
+        <div className={`${background} h-[102px] w-[102px] flex items-center justify-center`} >
+            <Image 
+            src={piece}
+            height={"-1".includes(pieceId) ? 60 : 80}
+            width={"-1".includes(pieceId) ? 60 : 80}
+            alt='Ola'
+            />
+        </div>
     )
 }
